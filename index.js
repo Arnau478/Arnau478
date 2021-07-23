81b402e26fbfe2a036de482a7bc93d554a28a893
Arnau478 <61841960+Arnau478@users.noreply.github.com>
2021-07-23 19:39:31 +0200

    Index page

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
+app.use('/public', express.static('public'));
 app.set('port', process.env.PORT || 80);
 
 app.listen(app.get('port'), () => console.log('Server running on port ' + app.get('port')));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
